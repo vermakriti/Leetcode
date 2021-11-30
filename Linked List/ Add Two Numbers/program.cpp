577d0149de0f3eec1ab414be6ed704e87ff3c18b
vermakriti <kriti.verma.it@gmail.com>
2021-11-30 11:15:25 +0530

    updated

--- Linked List/ Add Two Numbers/program.cpp
+++ Linked List/ Add Two Numbers/program.cpp
@@ -1,47 +1,58 @@
+/*
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+
+
+*/
+
+
+// TC-O(max(M,N)) SC-O(1)
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         
-        ListNode *curr1=l1,*curr2=l2,*head,*tail=new ListNode();
-        head=tail;
+        ListNode*tail=new ListNode(-1),*head=tail;
         int c=0;
-        
-        while(curr1 && curr2){
+        while(l1 || l2){
             
-            ListNode *node=new ListNode((curr1->val+curr2->val+c)%10);
-            c = (curr1->val+curr2->val+c)/10;
-            tail->next=node;
-            tail=node;
-            curr1=curr1->next;
-            curr2=curr2->next;
+            int n1= l1 ? l1->val :0;
+            int n2= l2 ? l2->val :0;
             
-        }
-        while(curr1){
-        
-            ListNode *node = new ListNode((curr1->val+c)%10);
-            c = (curr1->val+c)/10;
+            ListNode*node=new ListNode((n1+n2+c)%10);
+            c=(n1+n2+c)/10;
+            
             tail->next=node;
             tail=node;
-            curr1=curr1->next;
+            
+            if(l1)l1=l1->next;
+            if(l2)l2=l2->next;
         }
-        while(curr2){
-        
-            ListNode *node = new ListNode((curr2->val+c)%10);
-            c = (curr2->val+c)/10;
-            tail->next=node;
-            tail=node;
-            curr2=curr2->next;
-        }
-        
-        
         if(c){
-             ListNode *node = new ListNode(c);
-             tail->next=node;
+            ListNode*node=new ListNode(c);
+            tail->next=node;
+            tail=node;
         }
-
         head=head->next;
         return head;
-        
-       
     }
 };
